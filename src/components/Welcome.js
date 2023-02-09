15a00658c28c72e71b1b68869098ba0331e19d1a
Sambit Kumar Das <92737238+sambitkumar12@users.noreply.github.com>
2023-02-09 19:46:17 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
 // write code for Welcome component here
+import Welcome from './Welcome';
+
+const App = () => {
+  return (
+    <div>
+      <Welcome name="Sambit Kumar Das" />
+    </div>
+  );
+};
+
+export default App;
